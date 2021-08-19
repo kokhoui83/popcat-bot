0d03f37c080d0468a3f09c365688a051e10b7c25
brian lim <kokhoui.lim@duerrdental.com>
2021-08-19 09:33:02 +0800

    set headless and executablepath as const variable

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const puppeteer = require('puppeteer-core');
 
+const headless = false;
+const executablePath = '/opt/google/chrome/google-chrome';
+
 (async () => {
   const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-  const browser = await puppeteer.launch({ headless: false, executablePath: '/opt/google/chrome/google-chrome' });
+  const browser = await puppeteer.launch({ headless, executablePath });
   const page = await browser.newPage();
   await page.goto('https://popcat.click', { waitUntil: 'networkidle0' });
-  
+  // page.on('console', consoleObj => console.log(consoleObj.text()));
+
   while (true) {
-    await page.click('#app');
+    // click the cat
+    await page.click('.cat-img');
+    // check the click counter
+    // await page.$eval('.counter', el => { console.log(el.textContent) });
+    // wait before clicking again
     await delay(5);
   }
 
